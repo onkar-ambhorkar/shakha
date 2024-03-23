a148ab4cc824979321f5cbf7e6221f3ee644c8e7
onkar-ambhorkar <onkar.ambhorkar@gmail.com>
2024-03-23 18:40:38 +0530

    create app for guru govind singh shakha

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Create Project
 > django-admin startproject shakha
+
+# Create web app for Guru Govind Singh Shakha
+> python manage.py startapp guru_govind_singh
---
+++ guru_govind_singh/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GuruGovindSinghConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'guru_govind_singh'
