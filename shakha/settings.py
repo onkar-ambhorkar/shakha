bbeb94011c6765633c043811e2c2032424e2f206
onkar-ambhorkar <onkar.ambhorkar@gmail.com>
2024-03-31 11:54:51 +0530

    added env and requirement

--- shakha/settings.py
+++ shakha/settings.py
@@ -19,7 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y=ra(%mbuwge-9)f6odc-zuox=t+np8++h#av$uvsfcx^)dcwp'
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
