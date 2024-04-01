4419d19656d63644f79e9a6f088a807ea1e5d3a3
onkar-ambhorkar <onkar.ambhorkar@gmail.com>
2024-04-01 11:38:52 +0530

    bash update 5

--- shakha/settings.py
+++ shakha/settings.py
@@ -13,8 +13,6 @@ import os
 
 from pathlib import Path
 
-import dj_database_url
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,11 +80,10 @@ WSGI_APPLICATION = 'shakha.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/shakha',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
