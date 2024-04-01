be521e7ec8568b9e95916561b243e620fd17a5c4
onkar-ambhorkar <onkar.ambhorkar@gmail.com>
2024-04-01 13:10:44 +0530

    bash update 9

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 poetry install
 
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 ./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
